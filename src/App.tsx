9f9e1516bea08d9a7ff0ca92e8ee44784ddcbc70
Vlad <z0l0t0m@mail.ru>
2023-02-02 20:45:47 +0300

    add: route in Login component

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,6 @@ function App() {
             }
           />
         )}
-
         <Route
           path='*'
           element={<Navigate to={isLogIn ? "/app" : "/auth"} />}
--- src/components/login/LogIn.tsx
+++ src/components/login/LogIn.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SetPassword from "./SetPassword";
 import SetLogin from "./SetLogin";
+import { Routes, Route } from "react-router-dom";
 
 const LogIn = (props: any) => {
   const [isLoginEntered, setIsLoginEntered] = useState(false);
@@ -8,18 +9,32 @@ const LogIn = (props: any) => {
   const isLoginEnteredHandler = (handler: boolean) =>
     setIsLoginEntered(handler);
 
-  return isLoginEntered ? (
-    <SetPassword
-      isLogInHandler={props.isLogInHandler}
-      isLoginEnteredHandler={isLoginEnteredHandler}
-      passwordInputHandler={props.passwordInputHandler}
-      login={props.login}
-    />
-  ) : (
-    <SetLogin
-      isLoginEnteredHandler={isLoginEnteredHandler}
-      loginInputHandler={props.loginInputHandler}
-    />
+  return (
+    <Routes>
+      {isLoginEntered ? (
+        <Route
+          path=''
+          element={
+            <SetPassword
+              isLogInHandler={props.isLogInHandler}
+              isLoginEnteredHandler={isLoginEnteredHandler}
+              passwordInputHandler={props.passwordInputHandler}
+              login={props.login}
+            />
+          }
+        />
+      ) : (
+        <Route
+          path=''
+          element={
+            <SetLogin
+              isLoginEnteredHandler={isLoginEnteredHandler}
+              loginInputHandler={props.loginInputHandler}
+            />
+          }
+        />
+      )}
+    </Routes>
   );
 };
 
